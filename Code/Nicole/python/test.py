225fdd376324aba3e5db46d4d2ae3de47c854ef8
nicolesy <nicole@nicolesy.com>
2019-09-09 15:23:41 -0700

    :snake:

--- Code/Nicole/python/test.py
+++ Code/Nicole/python/test.py
@@ -1,9 +1,13 @@
 # test.py
 
-def missing_char(word):
-    word_list = []
-    for char in range(len(word)):
-        word_list.append(word[:char] + word[char + 1:])
-    return word_list
+def combine(nums1, nums2):
+    my_list = []
+    for i in range(len(nums1)):
+        my_list.append(nums1[i])
+        my_list.append(nums2[i])
+    return my_list
     
-print(missing_char("pizza"))
+l1 = ["a", "b", "c"]
+l2 = [1, 2, 3]
+
+print(combine(l1, l2))
