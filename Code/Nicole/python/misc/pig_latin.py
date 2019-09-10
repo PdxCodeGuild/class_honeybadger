64375bab45b0636397e0c87627c0f18537984aac
nicolesy <nicole@nicolesy.com>
2019-09-10 09:47:29 -0700

    :hamster:

---
+++ Code/Nicole/python/misc/pig_latin.py
@@ -0,0 +1,15 @@
+# pig_latin.py
+
+
+# convert string to list
+# determine if beginning starts with vowel
+# 
+
+def pig_latin(phrase):
+    phrase = phrase.split()
+    phrase = phrase.split()
+    return phrase
+
+
+user_input = input("Please type a phrase: ")
+print(pig_latin(user_input))
