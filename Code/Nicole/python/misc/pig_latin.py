5b9a93efbf421c68ddcf298b17100dec574e33c3
nicolesy <nicole@nicolesy.com>
2019-09-10 22:16:05 -0700

    :pig:

--- Code/Nicole/python/misc/pig_latin.py
+++ Code/Nicole/python/misc/pig_latin.py
@@ -11,35 +11,51 @@
 
 def pig_latin(phrase):
     phrase = phrase.split()
-    phrase_2 = phrase.split()
+    
+    
+    # phrase[list][word][letter]
+              # x   
     
     new_phrase = []
     pig_phrase = []
-    vowel = ["a", "e", "i", "o", "u", "w", "y"]
-    for i in range(len(phrase)):
-        new_phrase.append([phrase[i]]) # creates list of list with words from the phrase
-        
-        # for q in range(len(new_phrase)):
-        #     print(new_phrase[q])
-        #     new_phrase[i].append(new_phrase[q])
-        #     print(new_phrase)
-
-    #     for x in range(len(new_phrase)):
-    #         for y in range(len(new_phrase[x])):
-    #             if new_phrase[x][y][0] not in vowel:
-    #                 new_phrase.insert(new_phrase[x][y][-1], new_phrase[x][y][0])
-    #                 new_phrase.remove(new_phrase[x][y][0])
-    #                 new_phrase.insert(new_phrase[x][y][-1], "yay")
-    # 
-    # print(new_phrase)
-    # print(pig_phrase)
-    # return pig_phrase
+    
+    vowel = ["a", "e", "i", "o", "u", "y"]
+    punct = [".", ",", "'", "\"", "!", "?"]
+    th = ["th"]
+    
+    for x in range(len(phrase)):
+        new_phrase.append([phrase[x]]) # creates list of list with words from the phrase
+    for y in range(len(new_phrase)): 
+        #iterating through the sub-lists (words) in the list:
+        if new_phrase[y][0][-1] in punct: 
+            # this handles the punctuation properly
+            if new_phrase[y][0][0] in vowel:
+                pig_phrase.append(new_phrase[y][0][:-1] + "yay" + new_phrase[y][0][-1])
+            elif new_phrase[y][0][0] == "t" and new_phrase[y][0][1] == "h":
+                    pig_phrase.append(new_phrase[y][0][2:-1] + new_phrase[y][0][0:2] + "ay" + new_phrase[y][0][-1])
+            else:
+                # print(new_phrase[y][0][1:-1])
+                pig_phrase.append(new_phrase[y][0][1:-1] + new_phrase[y][0][0] + "ay" + new_phrase[y][0][-1])
+        # if no punctuation, this part goes:
+        elif new_phrase[y][0][0] in vowel:
+            pig_phrase.append(new_phrase[y][0][0:] + "yay")
+        # this handles words that start with "th"
+        elif new_phrase[y][0][0] == "t" and new_phrase[y][0][1] == "h":
+                pig_phrase.append(new_phrase[y][0][2:] + new_phrase[y][0][0:2] + "ay")
+        else:
+            pig_phrase.append(new_phrase[y][0][1:] + new_phrase[y][0][0] + "ay")
+            
+    return pig_phrase
 
 
 user_input = input("Please type a phrase: ").lower()
-print(pig_latin(user_input))
+print(*pig_latin(user_input))
 
-# my_list = [["this", "that", "other"], ["one", "two", "three"], ["my", "happy", "place"]]
+# my_list = [["this"], ["one"], ["my"]]
 # 
-# print(my_list[0])
-# print(my_list[0][0][0])
+# 
+# print(my_list)
+# print(my_list[0][0][0]) # first letter of first item in first list
+# print(my_list[0][0][1:] + my_list[0][0][0] + "ay") # consanent
+# print(my_list[1][0][0:] + "yay") # starts with a vowel
+# print(my_list[2][0][1:] + my_list[2][0][0] + "ay") # consanent
