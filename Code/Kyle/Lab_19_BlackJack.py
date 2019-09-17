f888cbbe4312d2bb7424af155add735c4ba7f1bf
Kyle <kyle@kylepc.localdomain>
2019-09-17 11:01:21 -0700

    added labs

--- Code/Kyle/Lab_19_BlackJack.py
+++ Code/Kyle/Lab_19_BlackJack.py
@@ -1,14 +1,33 @@
 
-
 Q = 10
-J = 10
 K = 10
 A = 1
+J = 10
 
-user_hand1 = int(input("Whats your first card? ")
-user_hand2 = int(input("Whats your second card? ")
-user_hand3 = int(input("Whats your third card? ")
+
+
+user_hand1 = float(input("Whats your first card? "))
+user_hand2= float(input("Whats your second card? "))
+user_hand3 = float(input("Whats your third card? "))
+
+hand_sum = [user_hand1, user_hand2, user_hand3]
 
 def add_user_hand():
-    added = user_hand1 + user_hand2 + user_hand3:
-return added
+    added = sum(hand_sum)
+    return added
+    print(added)
+
+add_user_hand()
+
+
+# def card_value():
+#     if user_input == "A":
+#         return 1
+#     elif user_input == "K":
+#         return 10
+#     elif user_input == "Q":
+#         return 10
+#     elif user_input == "J":
+#         return 10
+
+# card_value()
---
+++ Code/Kyle/Lab_23_Contacts_list.py
@@ -0,0 +1,7 @@
+
+
+
+
+with open('contacts.csv', 'r') as file:
+    lines = file.read().split('\n')
+    print(lines)
