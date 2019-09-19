977437ee0b895e05c73f32268da683f0cf597e52
Kyle <kyle@kylepc.localdomain>
2019-09-19 09:52:21 -0700

    deleted files

--- Code/Kyle/Lab_23_Contacts_list.py
+++ Code/Kyle/Lab_23_Contacts_list.py
@@ -5,3 +5,10 @@
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
     print(lines)
+    text = file.read()
+    print(text)
+
+    contacts = []
+    text = text.split("\n")
+    print(text)
+    
--- Code/Kyle/Lab_24_raindata.py
+++ Code/Kyle/Lab_24_raindata.py
@@ -1,28 +1,26 @@
 import requests
 import re
-import datetime
+import datetime as d
 import math
+import datetime
+
+
+load_data = requests.get('https://or.water.usgs.gov/non-usgs/bes/columbia_ips.rain')
 
 
 
+def data_extraction():
 
-data_table = requests.get('https://or.water.usgs.gov/non-usgs/bes/columbia_ips.rain')
+    date = datetime.datetime.strptime('16-JUN-2000', '%d-%b-%Y')
+    page = load_data.text
+    data = re.findall('(\d{2}-\w{3}-\d{4})\s+(\d+)', page)
+    data_table = []
 
+    for i in range(len(data)):
+        date = data[i][0]
+        rain = data[i][1]
+        date2 = d.datetime.strptime(date, '%d-%b-%Y')
+        data_table.append({'year': date2.year, 'month': date2.month, 'day': date2.day, 'rain': rain})
+    return data_table
 
-date = datetime.datetime.strptime('16-JUN-2000', '%d-%b-%Y')
-page = data_table.text
-data = re.findall('(\d{2}-\w{3}-\d{4})\s+(\d+)', page)
-# print(data_table)
-
-for row in data:
-    # row = data[i]
-    date = datetime.datetime.strptime(row[0], '%d-%b-%Y')
-    daily_total = int(row[1])
-    #convert = datetime.datetime.strptime('date', '%d-%b-%Y')
-    print(date.year, date.month, date.day, daily_total)
-
-    sum = 0
-    for num in row[1]:
-        sum = sum + daily_total
-
-    print(f'the sum is', sum)
+data_extraction()
