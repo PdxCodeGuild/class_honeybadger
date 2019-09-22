e3785fbb8477d4cc943a79386aff1f1e7c9421ea
boss hoss <judy.combs.reyes@gmail.com>
2019-09-21 23:47:25 -0700

    9/21/19

--- Code/Judith/problem_set2.py
+++ Code/Judith/problem_set2.py
@@ -4,6 +4,7 @@ import random
 import string
 
 #problem 1
+# Get a string from the user, print out another string, doubling every letter.
 """phrase = input('phrase')
 splitphrase = list(phrase)
 dubphrase = ''
@@ -12,6 +13,7 @@ for i in range(len(splitphrase)):
 print(dubphrase)"""
 
 #problem 2
+# Write a function that takes a string, and returns a list of strings, each missing a different character.
 """phrase = input()
 def remr(phrase):
     splitphrase = list(phrase)
@@ -23,6 +25,7 @@ for i in range(len(phrase)):
     print(remr(phrase))"""
 
 #problem 3
+# Return the letter that appears the latest in the english alphabet.
 """letters = string.ascii_lowercase
 phrase = input('phrase...')
 def findlast(phrase,letters):
@@ -36,6 +39,7 @@ def findlast(phrase,letters):
 print(letters[findlast(phrase,letters)])"""
 
 #problem 4
+# Write a function that returns the number of occurances of 'hi' in a given string.
 """phrase = input('phrase...')
 count = 0
 while 'hi' in phrase:
@@ -44,6 +48,7 @@ while 'hi' in phrase:
 print(count)"""
 
 #problem 5
+# Write a function that returns True if a given string contains the same number of 'cat' as it does 'dog'
 """phr = input('phrase...')
 def chk(phr):
     if phr.count('cat') == phr.count('dog'):
@@ -53,11 +58,13 @@ def chk(phr):
 print(chk(phr))"""
 
 #problem 6
+# Return the number of letter occurances in a string.
 """phr = input('phrase...')
 letter = input('letter')
 print(phr.count(letter))"""
 
 #problem 7
+# Convert input strings to lowercase without any surrounding whitespace.
 """phr = input('phrase...')
 phr = phr.replace(' ','') 
 phr = phr.lower() 
