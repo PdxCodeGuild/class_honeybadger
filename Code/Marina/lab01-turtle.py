4371c4b184985dff49f8818af05eec8f555843a8
Marina Poltorak <emylisfamily@gmail.com>
2019-09-24 15:31:21 -0700

    add files

--- Code/Marina/lab01-turtle.py
+++ Code/Marina/lab01-turtle.py
@@ -1,55 +1,87 @@
 from turtle import *
+#
+# fillcolor('red')
+# begin_fill()
+# # body
+# left(90)
+# forward(90)
+# forward(90)
+#
+# left(45)
+# forward(45)
+# right(45)
+# forward(45)
+# right(45)
+# forward(45)
+# right(45)
+# forward(45)
+# right(45)
+# forward(45)
+# right(45)
+# forward(45)
+# right(45)
+# forward(45)
+# left(45)
+# forward(180)
+# left(45)
+# forward(90)
+# right(180)
+# forward(90)
+# left(45)
+# forward(45)
+# left(45)
+# forward(90)
+# right(180)
+# forward(90)
+# left(45)
+# forward(120)
+# left(90)
+# forward(90)
+# right(180)
+# forward(90)
+# right(90)
+# forward(120)
+# left(90)
+# forward(45)
+#
+# end_fill()
+#
+# left(90)
+# forward(120)
+# right(90)
+# forward(90)
+#
+# # edge_length = 0
+# # i = 0
+# # while i < 100:
+# # 	forward(edge_length)
+# # 	right(144)
+# #
+# # 	edge_length += 4
+#
+# done()
+#
+# i = 0
+# while i < 4:
+#     forward(100)
+#     left(90)
+#     i = i + 1
+#
+# done()
 
-fillcolor('red')
+fillcolor('pink')
+
+n_sides = 12
+edge_length = 0
+
+i = 0
 begin_fill()
-# body
-left(90)
-forward(90)
-forward(90)
-
-left(45)
-forward(45)
-right(45)
-forward(45)
-right(45)
-forward(45)
-right(45)
-forward(45)
-right(45)
-forward(45)
-right(45)
-forward(45)
-right(45)
-forward(45)
-left(45)
-forward(180)
-left(45)
-forward(90)
-right(180)
-forward(90)
-left(45)
-forward(45)
-left(45)
-forward(90)
-right(180)
-forward(90)
-left(45)
-forward(120)
-left(90)
-forward(90)
-right(180)
-forward(90)
-right(90)
-forward(120)
-left(90)
-forward(45)
-
+while i < 150:
+	forward(edge_length)
+	right(360/n_sides)
+	i = i + 1
+	edge_length = edge_length + 1
 end_fill()
-
-left(90)
-forward(120)
-right(90)
-forward(90)
 
 # edge_length = 0
 # i = 0
@@ -58,5 +90,4 @@ forward(90)
 # 	right(144)
 #
 # 	edge_length += 4
-
 done()
