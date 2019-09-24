f5194b44ec6979646470742907393bd3b7ad39ad
Marina Poltorak <emylisfamily@gmail.com>
2019-09-24 16:02:15 -0700

    editing labs

--- Code/Marina/lab16-image_manipulation2.py
+++ Code/Marina/lab16-image_manipulation2.py
@@ -8,11 +8,11 @@ pixels = img.load()
 for i in range(width):
     for j in range(height):
         r, g, b = pixels[i, j]
-        h, s, v = colorsys.rgb_to_hsb(r/255, g/255, b/255)
+        h, s, v = colorsys.rgb_to_hsv(r/255, g/255, b/255)
         r, g, b = colorsys.hsv_to_rgb(h, s, v)
-        r = int(r*255)
-        g = int(g*255)
-        b = int(b*255)
+        r = int(r*115)
+        g = int(g*300)
+        b = int(b*115)
         pixels[i, j] = (r, g, b)
 
 img.show()
