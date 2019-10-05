0bcb39ae1cc056843357743e670da5987e13a581
nicolesy <nicole@nicolesy.com>
2019-10-05 09:13:11 -0700

    Test file for js

---
+++ Code/Nicole/javascript/test.js
@@ -0,0 +1,10 @@
+
+// Doing math
+let sum = 10 + 10;
+let difference = 10 - 5;
+let product = 8 * 2;
+
+// This shows the result in the browser console
+console.log(sum)
+console.log(difference)
+console.log(product)
