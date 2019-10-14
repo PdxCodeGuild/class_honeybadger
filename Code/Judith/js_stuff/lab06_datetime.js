0deaec43dab70585b42778852224a0aa7f74a99b
boss hoss <judy.combs.reyes@gmail.com>
2019-10-13 19:00:48 -0700

    10/13/19

--- Code/Judith/js_stuff/lab06_datetime.js
+++ Code/Judith/js_stuff/lab06_datetime.js
@@ -21,14 +21,24 @@ const clearBtn = document.querySelector("#clearBtn")
 
 const saveList = document.querySelector("#saveList")
 
+const countDownInputsHrs = document.querySelector("#countDownInputsHrs")
+
+const countDownInputsMins = document.querySelector("#countDownInputsMins")
+
+const countDownInputsSecs = document.querySelector("#countDownInputsSecs")
+
+const countDownDisplay = document.querySelector("#countDownDisplay")
+
+const countDownStartBtn = document.querySelector("#countDownStartBtn")
+
 function keyWatch() {
     const total = Date.now() // milliseconds since Jan. 1st, 1970 in UST
-    const milliseconds = total % 1000
-    const seconds = Math.floor(total/1000) % 60
-    const minutes = Math.floor(total/60000) % 60
-    const hours = (Math.floor(total/3600000) + 17) % 24 //set to PST
+    const milliseconds = total
+    const seconds = Math.floor(total/1000)
+    const minutes = Math.floor(total/60000)
+    const hours = (Math.floor(total/3600000) + 17) //set to PST
     const days = Math.floor(total/86400000) % 365
-    const years = Math.floor(total/31536000000) + 1970
+    const years = Math.floor(total/31536000000)
 
     let timeArray = [milliseconds,seconds,minutes,hours,days,years]
     return timeArray
@@ -37,12 +47,12 @@ function keyWatch() {
 
 function displayUpdate(timeArray) {
     
-    msDisplay.innerText = timeArray[0]
-    secDisplay.innerText = timeArray[1]
-    minDisplay.innerText = timeArray[2]
-    hrDisplay.innerText = timeArray[3]
-    dayDisplay.innerText = timeArray[4]
-    yrDisplay.innerText = timeArray[5]
+    msDisplay.innerText = timeArray[0] % 1000
+    secDisplay.innerText = timeArray[1] % 60
+    minDisplay.innerText = timeArray[2] % 60
+    hrDisplay.innerText = timeArray[3] % 24
+    dayDisplay.innerText = timeArray[4] % 365
+    yrDisplay.innerText = timeArray[5] + 1970
 
 }
 
@@ -55,35 +65,64 @@ function stopWatch() {
         return
     }
     
-    timeStart = Date.now() //keyWatch()
-    // const msStart = timeStart[0]
-    // const secStart = timeStart[1]
-    // const minStart = timeStart[2]
+    let timeStart = masterTime[0]
 
     interval = setInterval(function() {
-        // let time = keyWatch()
-        let diff = Date.now()-timeStart
+        let diff = masterTime[0]-timeStart
 
-        // const ms = time[0]
-        // const sec = time[1]
-        // const min = time[2]
-        // stopDisplay.innerText = `${min-minStart}:${sec-secStart}:${ms-msStart}`
-        const milliseconds = diff
+        const milliseconds = diff % 1000
         const seconds = Math.floor(diff/1000) % 60
         const minutes = Math.floor(diff/60000) % 60
         stopDisplay.innerText = `${minutes}:${seconds}:${milliseconds}`
     
 
     }, 1)
-
-    
-
-    
+   
 }
 
+function countDown() {
+    const runHrs = countDownInputsHrs.value * 3600
+    const runMins = countDownInputsMins.value * 60
+    const runSecs = countDownInputsSecs.value
+
+    runTime = runHrs + runMins + runSecs
+
+    let timeStart = masterTime[1]
+
+    let runFlag = true
+
+    while (runFlag) {   
+        interval = setInterval(function() {
+            let diff = masterTime[1]-timeStart
+            
+            let curTime = runTime - diff 
+
+            const seconds = Math.floor(diff/1000)
+            const minutes = Math.floor(diff/60000)
+            const hours = Math.floor(diff/3600000)
+
+            const endSecs = (runSecs - seconds)
+            const endMins = (runMins - minutes)
+            const endHrs = runHrs - hours
+            countDownDisplay.innerText = `${endHrs}:${endMins}:${endSecs}`
+            
+            if (curTime == 0) {
+                runFlag = false
+            }
+
+        }, 1000)
+    }
+
+}
+
+let masterTime
 setInterval(function() {
-    displayUpdate(keyWatch())
-}, 1)
+    masterTime = keyWatch()
+},1)
+
+setInterval(function() {
+    displayUpdate(masterTime)
+},1)
 
 stopBtn.addEventListener("click", function() {
     stopWatch()
@@ -103,4 +142,8 @@ saveBtn.addEventListener("click", function() {
 
 clearBtn.addEventListener("click", function() {
     saveList.innerHTML = ''
+})
+
+countDownStartBtn.addEventListener("click", function() {
+    countDown()
 })
