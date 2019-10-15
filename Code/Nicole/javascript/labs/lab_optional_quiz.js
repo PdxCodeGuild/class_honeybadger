7ff745cbcef1213e6be51a8e52f570652105f182
Nicole S. Young <nicolesy@iMac-2.local>
2019-10-15 16:13:09 -0700

    Trying to get HTML to display with appendChild

--- Code/Nicole/javascript/labs/lab_optional_quiz.js
+++ Code/Nicole/javascript/labs/lab_optional_quiz.js
@@ -1,20 +1,6 @@
 let title = document.querySelector("#title")
 let question = document.querySelector("#question")
 let answers = document.querySelector("#answers")
-let a1 = document.querySelector("#a1")
-let a2 = document.querySelector("#a2")
-let a3 = document.querySelector("#a3")
-let a4 = document.querySelector("#a4")
-let a5 = document.querySelector("#a5")
-let a6 = document.querySelector("#a6")
-
-// radio inputs
-let radio_a1 = document.querySelector("#radio_a1")
-let radio_a2 = document.querySelector("#radio_a2")
-let radio_a3 = document.querySelector("#radio_a3")
-let radio_a4 = document.querySelector("#radio_a4")
-let radio_a5 = document.querySelector("#radio_a5")
-let radio_a6 = document.querySelector("#radio_a6")
 
 // buttons
 let btn_submit = document.querySelector("#btn_submit")
@@ -24,130 +10,124 @@ let btn_begin = document.querySelector("#btn_begin")
 
 
 let quiz = {
-    title: "JavaScript Quiz",
-    questions: [{
-        text: "How do you increment a number in JavaScript?",
-        answers: [{
-            text: "i = i + 1",
-            correct: false
-        }, {
-            text: "i += 1",
-            correct: false
-        }, {
-            text: "i++",
-            correct: false
-        }, {
-            text: "++i",
-            correct: false
-        }, {
-            text: "i -= -1",
-            correct: false
-        }, {
-            text: "all of the above",
-            correct: true
-        }]
+  title: "JavaScript Quiz",
+  questions: [{
+    text: "How do you increment a number in JavaScript?",
+    answers: [{
+      text: "i = i + 1",
+      correct: false
     }, {
-        text: "In what order are the parts of a for loop?",
-        answers: [{
-            text: "initialization, condition, increment",
-            correct: true
-        }, {
-            text: "condition, initialization, increment",
-            correct: false
-        }, {
-            text: "initialization, increment, condition",
-            correct: false
-        }, {
-            text: "condition, initialization, increment",
-            correct: false
-        }]
+      text: "i += 1",
+      correct: false
     }, {
-        text: "What's the difference between == and ===?",
-        answers: [{
-            text: "nothing",
-            correct: false
-        }, {
-            text: "== coerces types, === does not",
-            correct: true
-        }, {
-            text: "=== coerces types, == does not",
-            correct: false
-        }]
+      text: "i++",
+      correct: false
+    }, {
+      text: "++i",
+      correct: false
+    }, {
+      text: "i -= -1",
+      correct: false
+    }, {
+      text: "all of the above",
+      correct: true
     }]
+  }, {
+    text: "In what order are the parts of a for loop?",
+    answers: [{
+      text: "initialization, condition, increment",
+      correct: true
+    }, {
+      text: "condition, initialization, increment",
+      correct: false
+    }, {
+      text: "initialization, increment, condition",
+      correct: false
+    }, {
+      text: "condition, initialization, increment",
+      correct: false
+    }]
+  }, {
+    text: "What's the difference between == and ===?",
+    answers: [{
+      text: "nothing",
+      correct: false
+    }, {
+      text: "== coerces types, === does not",
+      correct: true
+    }, {
+      text: "=== coerces types, == does not",
+      correct: false
+    }]
+  }]
 }
 
 title.innerHTML = quiz.title
 
-answers.style.display = "none"
-question.style.display = "none"
-btn_submit.style.display = "none"
-btn_paginate_next.style.display = "none"
-btn_paginate_previous.style.display = "none"
-
+window.addEventListener("load", function() {
+  answers.style.display = "none"
+  question.style.display = "none"
+  btn_submit.style.display = "none"
+  btn_paginate_next.style.display = "none"
+  btn_paginate_previous.style.display = "none"
+})
 
 btn_begin.addEventListener("click", function() {
-    btn_begin.style.display = "none"
-    question.style.display = null
-    answers.style.display = null
-    btn_submit.style.display = null
-    btn_paginate_next.style.display = null
-    btn_paginate_previous.style.display = null
+  btn_begin.style.display = "none"
+  question.style.display = null
+  answers.style.display = null
+  btn_submit.style.display = null
+  btn_paginate_next.style.display = null
+  btn_paginate_previous.style.display = null
 })
 
 
-function generateQuestions() {
-    for (let i = 0; i < (quiz.questions).length; ++i) {
-        question.innerHTML = quiz.questions[i].text
-        if (quiz.questions[i].answers[i].text != "") {
-            radio_a1.style.display = null
-            a1.innerHTML = quiz.questions[i].answers[i].text
-        } else {
-            radio_a1.style.display = "none"
-        }
-        if (quiz.questions[i].answers[i + 1].text != "") {
-            radio_a2.style.display = null
-            a2.innerHTML = quiz.questions[i].answers[i + 1].text
-        } else {
-            radio_a2.style.display = "none"
-        }
-        if (quiz.questions[i].answers[i + 2].text != "") {
-            radio_a3.style.display = null
-            a3.innerHTML = quiz.questions[i].answers[i + 2].text
-        } else {
-            radio_a3.style.display = "none"
-        }
-        if (quiz.questions[i].answers[i + 3].text != "") {
-            radio_a4.style.display = null
-            a4.innerHTML = quiz.questions[i].answers[i + 3].text
-        } else {
-            radio_a4.style.display = "none"
-        }
-        if (quiz.questions[i].answers[i + 4].text != null) {
-            radio_a5.style.display = null
-            a5.innerHTML = quiz.questions[i].answers[i + 4].text
-        } else {
-            radio_a5.style.display = "none"
-        }
-        if (quiz.questions[i].answers[i + 5].text) {
-            radio_a5.style.display = null
-            a6.innerHTML = quiz.questions[i].answers[i + 5].text
-        } else {
-            radio_a6.style.display = "none"
-        }
-        i++
 
-    }
+function getAnswers(x) {
+  for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
+    answers.style.display = "block"
+    question.innerHTML = quiz.questions[x].text
 
-    btn_paginate_next.addEventListener("click", function() {
-                for (let i = 1; i < (quiz.questions).length; ++i) {
-                    generateQuestions(i)
+    let label = document.createElement("LABEL")
+    label.setAttribute("id", "label")
+    answers.appendChild(label)
 
-                }
-            }
+    let input = document.createElement("INPUT")
+    input.setAttribute("id", "input")
+    label.appendChild(input)
+    input.setAttribute("type", "radio")
+    input.setAttribute("name", "radio_answer")
+    input.setAttribute("class", "answer_choice")
+
+    let span = document.createElement("SPAN")
+    span.setAttribute("id", "span")
+    input.appendChild(span)
+    span.innerHTML = quiz.questions[x].answers[i].text
+
+    console.log((quiz.questions[x].answers).length) //6
+    console.log(label)
+    console.log(input)
+    console.log(span)
+  }
+}
+
+getAnswers(0)
+
+
+// btn_paginate_next.addEventListener("click", function() {
+//   for (let i = 1; i < (quiz.questions).length; ++i) {
+//     getAnswers(i)
+// 
+//   }
+// })
 
 
 
 
-            // console.log(quiz.questions[0].text)
-            // question.innerHTML = quiz.questions[0].text
-            // a1.innerHTML = quiz.questions[0].answers[0].text
+
+
+
+
+// console.log(quiz.questions[0].text)
+// question.innerHTML = quiz.questions[0].text
+// a1.innerHTML = quiz.questions[0].answers[0].text
