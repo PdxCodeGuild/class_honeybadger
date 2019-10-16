dd1175156e218a24c01484c89ac893307b08b97c
nicolesy <nicole@nicolesy.com>
2019-10-16 16:31:27 -0700

    Got the checkbox to register (needs more updates and styling)

--- Code/Nicole/javascript/labs/lab_optional_quiz.js
+++ Code/Nicole/javascript/labs/lab_optional_quiz.js
@@ -2,6 +2,11 @@ let title = document.querySelector("#title")
 let question = document.querySelector("#question")
 let answers = document.querySelector("#answers")
 let question_area = document.querySelector("#question_area")
+let true_or_false = document.querySelector("#true_or_false")
+
+let score_area = document.querySelector("#score_area")
+let score = document.querySelector("#score")
+let question_length = document.querySelector("#question_length")
 
 // buttons
 let btn_submit = document.querySelector("#btn_submit")
@@ -11,158 +16,181 @@ let btn_view_score = document.querySelector("#btn_view_score")
 
 
 let quiz = {
-  title: "JavaScript Quiz",
-  questions: [{
-    text: "How do you increment a number in JavaScript?",
-    answers: [{
-      text: "i = i + 1",
-      correct: false
+    title: "JavaScript Quiz",
+    questions: [{
+        text: "How do you increment a number in JavaScript?",
+        answers: [{
+            text: "i = i + 1",
+            correct: false
+        }, {
+            text: "i += 1",
+            correct: false
+        }, {
+            text: "i++",
+            correct: false
+        }, {
+            text: "++i",
+            correct: false
+        }, {
+            text: "i -= -1",
+            correct: false
+        }, {
+            text: "all of the above",
+            correct: true
+        }]
     }, {
-      text: "i += 1",
-      correct: false
+        text: "In what order are the parts of a for loop?",
+        answers: [{
+            text: "initialization, condition, increment",
+            correct: true
+        }, {
+            text: "condition, initialization, increment",
+            correct: false
+        }, {
+            text: "initialization, increment, condition",
+            correct: false
+        }, {
+            text: "condition, initialization, increment",
+            correct: false
+        }]
     }, {
-      text: "i++",
-      correct: false
-    }, {
-      text: "++i",
-      correct: false
-    }, {
-      text: "i -= -1",
-      correct: false
-    }, {
-      text: "all of the above",
-      correct: true
+        text: "What's the difference between == and ===?",
+        answers: [{
+            text: "nothing",
+            correct: false
+        }, {
+            text: "== coerces types, === does not",
+            correct: true
+        }, {
+            text: "=== coerces types, == does not",
+            correct: false
+        }]
     }]
-  }, {
-    text: "In what order are the parts of a for loop?",
-    answers: [{
-      text: "initialization, condition, increment",
-      correct: true
-    }, {
-      text: "condition, initialization, increment",
-      correct: false
-    }, {
-      text: "initialization, increment, condition",
-      correct: false
-    }, {
-      text: "condition, initialization, increment",
-      correct: false
-    }]
-  }, {
-    text: "What's the difference between == and ===?",
-    answers: [{
-      text: "nothing",
-      correct: false
-    }, {
-      text: "== coerces types, === does not",
-      correct: true
-    }, {
-      text: "=== coerces types, == does not",
-      correct: false
-    }]
-  }]
 }
 
 title.innerHTML = quiz.title
 
 window.addEventListener("load", function() {
-  answers.style.display = "none"
-  question.style.display = "none"
-  btn_submit.style.display = "none"
-  btn_paginate_next.style.display = "none"
-  btn_view_score.style.display = "none"
+    answers.style.display = "none"
+    question.style.display = "none"
+    btn_submit.style.display = "none"
+    btn_paginate_next.style.display = "none"
+    btn_view_score.style.display = "none"
+    score_area.style.display = "none"
 })
 
 btn_begin.addEventListener("click", function() {
-  btn_begin.style.display = "none"
-  question.style.display = null
-  answers.style.display = null
-  btn_submit.style.display = null
-  btn_paginate_next.style.display = null
+    btn_begin.style.display = "none"
+    question.style.display = null
+    answers.style.display = null
+    btn_paginate_next.style.display = null
 })
 
 let label = document.createElement("LABEL")
 let span = document.createElement("SPAN")
 let input = document.createElement("INPUT")
 
+score_num = 0
 
 // this works --------->
 function getAnswers(x) {
-  for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
-    answers.style.display = "block"
-    answers.className = "posted"
-    question.innerHTML = quiz.questions[x].text
-
-    let label = document.createElement("LABEL")
-    label.setAttribute("id", "answer_label")
-    label.setAttribute("for", "answer_input_" + i)
-
-    let span = document.createElement("SPAN")
-    span.setAttribute("id", "answer_span")
-    span.setAttribute("for", "answer_input_" + i)
-
-    let input = document.createElement("INPUT")
-    input.setAttribute("id", "answer_input_" + i)
-    input.setAttribute("type", "radio")
-    input.setAttribute("name", "radio_answer")
-    input.setAttribute("class", "answer_choice")
-    input.setAttribute("checked", "checked")
-    input.setAttribute("value", quiz.questions[x].answers[i].correct)
-
-    answers.appendChild(label)
-    label.appendChild(input)
-    label.appendChild(span)
-    span.innerHTML = quiz.questions[x].answers[i].text
-
-  }
-
-  // this determines if the answer is true or false
-  btn_submit.addEventListener("click", function() {
     for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
-      if (quiz.questions[x].answers[i].correct == answers.checked) {
-        console.log("hello!")
-      }
-    }
-    // if (input.value.checked == true) {
-    //   console.log("true")
-    // } else {
-    //   console.log("false")
-    // }
-  })
+        answers.style.display = "block"
+        answers.className = "posted"
+        question.innerHTML = quiz.questions[x].text
 
-  answers.addEventListener("change", function() {
-    // for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
-    //   let is_checked = document.getElementById("answer_input_" + i).value
-    //   if (quiz.questions[x].answers[i].correct == is_checked.checked) {
-    //     console.log("hello!")
-    //   }
+        let label = document.createElement("LABEL")
+        label.setAttribute("id", "answer_label")
+        label.setAttribute("for", "answer_input_" + i)
+
+        let span = document.createElement("SPAN")
+        span.setAttribute("id", "answer_span")
+        span.setAttribute("for", "answer_input_" + i)
+
+        let input = document.createElement("INPUT")
+        input.setAttribute("id", "answer_input_" + i)
+        input.setAttribute("type", "radio")
+        input.setAttribute("name", "radio_answer")
+        input.setAttribute("class", "answer_choice")
+        // input.setAttribute("checked", "checked")
+        input.setAttribute("value", quiz.questions[x].answers[i].correct)
+        // input.dataset.correct = quiz.questions[x].answers[i].correct
+
+
+        input.addEventListener("change", function() {
+            score_area.style.display = null
+            console.log(input.value)
+            if (input.value == "true") {
+                true_or_false.innerText = "That is correct!"
+                    ++score_num
+            } else {
+                true_or_false.innerText = "That is incorrect"
+            }
+            score.innerHTML = score_num
+            console.log(score)
+            question_length.innerHTML = quiz.questions.length
+        })
+
+
+        answers.appendChild(label)
+        label.appendChild(input)
+        label.appendChild(span)
+        span.innerHTML = quiz.questions[x].answers[i].text
+
+    }
+
+    // this determines if the answer is true or false DOES NOT WORK YET
+    btn_submit.addEventListener("click", function() {
+        for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
+            if (quiz.questions[x].answers[i].correct == answers.checked) {
+                console.log("hello!")
+            }
+        }
+        // if (input.value.checked == true) {
+        //   console.log("true")
+        // } else {
+        //   console.log("false")
+        // }
+    })
+
+    // answers.addEventListener("change", function() {
+    //     // for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
+    //     //   let is_checked = document.getElementById("answer_input_" + i).value
+    //     //   if (quiz.questions[x].answers[i].correct == is_checked.checked) {
+    //     //     console.log("hello!")
+    //     //   }
+    //     // 
+    //     // }
+    //     for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
+    //         let is_checked = quiz.questions[x].answers[i].correct
+    //         console.log(input.checked)
+    //         if (is_checked == input.checked) {
+    //             console.log("hello!")
+    //         }
     // 
-    // }
-    for (let i = 0; i < (quiz.questions[x].answers).length; ++i) {
-      let is_checked = quiz.questions[x].answers[i].correct
-      console.log(input.checked)
-      if (is_checked == input.checked) {
-        console.log("hello!")
-      }
-
-    }
-  })
+    //     }
+    // })
 }
+
 
 
 question_i = 0
 
 function nextQuestion() {
-  question.innerHTML = ""
-  answers.innerHTML = ""
-  if (question_i == (quiz.questions).length) question_i = 0; {
+    question.innerHTML = ""
+    answers.innerHTML = ""
+    if (question_i == quiz.questions.length) {
+        question_i = 0;
+    }
     getAnswers(question_i)
-      ++question_i
-  }
-  if (question_i == (quiz.questions).length) {
-    console.log("yoyo")
-  }
+        ++question_i
+
+    if (question_i == (quiz.questions).length) {
+        btn_paginate_next.style.display = "none"
+        btn_submit.style.display = null
+    }
 }
+
 
 
 
