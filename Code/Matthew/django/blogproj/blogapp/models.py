4679df0c01fa38c0792eb30413fac8410921dbf6
flux2341 <flux2341@gmail.com>
2019-10-21 15:14:20 -0700

    blog project in django

---
+++ Code/Matthew/django/blogproj/blogapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class BlogPostType(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    type = models.ForeignKey(BlogPostType, on_delete=models.PROTECT)
+    date_created = models.DateTimeField()
+    
+    def pretty_date(self):
+        return self.date_created.strftime('%B %d')
+
+    def __str__(self):
+        return self.title
+
+
