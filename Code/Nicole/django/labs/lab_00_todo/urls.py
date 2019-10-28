f2154410dec847827a4a6852126de440e2d64e65
nicolesy <nicole@nicolesy.com>
2019-10-28 16:25:11 -0700

    Finished todo lab

--- Code/Nicole/django/labs/lab_00_todo/urls.py
+++ Code/Nicole/django/labs/lab_00_todo/urls.py
@@ -6,5 +6,9 @@ from . import views
 app_name = "lab_00_todo"
 
 urlpatterns = [
-    path("", views.index, name="index"),    
+    path("", views.index, name="index"),
+    path("complete_todo/", views.complete_todo, name="complete_todo"),
+    path("add_todo/", views.add_todo, name="add_todo"),
+    path("delete_todo/", views.delete_todo, name="delete_todo"),
+    path("undo_todo/", views.undo_todo, name="undo_todo"),
 ]
